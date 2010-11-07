43f4a68629a498174a0b3802addba4fd9358e314
vis81 <vis81@mail.ru>
2010-11-07 21:45:26 +0200

    cleanup

--- build/build.bat
+++ build/build.bat
@@ -1,12 +1,15 @@
 SET ARMDIR=C:\Keil\ARM
 SET MINGWDIR=C:\Program Files\CodeBlocks\MinGW
-
 mkdir ..\out
+
+
 cd fw
 call flash.bat
-cd ..\app
-call make.bat
-
 cd ..
+
+cd app
+call make.bat
+cd ..
+
 mkdir ..\out\drv
 xcopy /S /Y drv ..\out\drv
